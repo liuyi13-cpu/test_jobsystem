5ffa6af14cf79517ed940dc6d9ad63e5a8eaf5cf
wangxueping03 <wangxueping03@kuaishou.com>
2024-08-26 22:03:23 +0800

    * init

---
+++ Assets/Scripts/TestJobs.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJobs : MonoBehaviour
+{
+    private void Start()
+    {
+        // TODO
+    }
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Util
+{
+}
